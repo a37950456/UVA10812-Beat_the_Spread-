3796704be5f78fa03135b159134560e7f794307a
ChiaTien <a37950456@yahoo.com.tw>
2018-05-17 17:04:24 +0800

    Initial Commit

---
+++ UVA10812-Beat_the_Spread!/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UVA10812-Beat_the_Spread!
+//
+//  Created by Tina Tsai on 2018/5/17.
+//  Copyright © 2018年 Tina Tsai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
